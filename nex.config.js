d7b63d1fd2ae5493859c861f9baa2419afb6e136
Kishore V <kishore@Kishores-MacBook-Pro.local>
2024-09-28 12:22:46 +0530

    Sign in with google added

---
+++ nex.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['lh3.googleusercontent.com'], // Add other domains if needed
+    },
+}
+
+module.exports = nextConfig
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,30 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google"
+//
+// export default NextAuth({
+//     providers: [
+//         GoogleProvider({
+//             clientId: '558985239679-t4qtlue8i7c9ht59e0kpmfhvhv9ohnh5.apps.googleusercontent.com',
+//             clientSecret: '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',
+//             authorization: {
+//                 params: {
+//                     prompt: "select_account",  // Forces Google to always show the account selection
+//                 },
+//             },
+//         }),
+//     ],
+// });
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '558985239679-t4qtlue8i7c9ht59e0kpmfhvhv9ohnh5.apps.googleusercontent.com',
-            clientSecret: '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',
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             authorization: {
                 params: {
-                    prompt: "select_account",  // Forces Google to always show the account selection
+                    prompt: "select_account",
                 },
             },
         }),
